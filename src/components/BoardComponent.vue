1cce31817f05457adf2ec852f232632a339cf2c2
Danila <devdaniellle@gmail.com>
2024-04-21 00:14:31 +0500

    added selecting cell with figure

--- src/components/BoardComponent.vue
+++ src/components/BoardComponent.vue
@@ -1,20 +1,44 @@
 <template>
   <div v-if="props.board !== null" class="board">
     <div v-for="row in props.board.cells" :key="row[0].id">
-      <CellComponent v-for="cell in row" :key="cell.id" class="cell" :cell="cell"> </CellComponent>
+      <CellComponent
+        v-for="cell in row"
+        :key="cell.id"
+        class="cell"
+        :cell="cell"
+        :selected="cell.x === selectedCell?.x && cell.y === selectedCell?.y"
+        :click="click"
+      >
+      </CellComponent>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { Board } from '/src/models/Board.ts'
 import CellComponent from './CellComponent.vue'
+import { Cell } from '@/models/Cell'
 
 interface BoardProps {
   board: Board
 }
 
 const props: BoardProps = defineProps<BoardProps>()
+
+const selectedCell = ref<Cell | null>(null)
+
+const setSelectedCell = (cell: Cell | null) => {
+  selectedCell.value = cell
+}
+
+function click(cell: Cell) {
+  if (cell.figure) {
+    setSelectedCell(cell)
+  }
+}
+
+
 </script>
 
 <style scoped></style>
--- src/components/CellComponent.vue
+++ src/components/CellComponent.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="cell" :class="props.cell.color">
+  <div
+    @click="click(cell)"
+    class="cell"
+    :class="[props.cell.color, { selected: props.selected }]"
+    :style="{ background: cell.available && cell.figure ? '#7B61FF' : '' }"
+  >
+    <div v-if="!cell.figure && cell.available" class="available"></div>
     <img v-if="props.cell.figure" :src="props.cell.figure.icon" alt="" />
   </div>
 </template>
@@ -9,8 +15,9 @@ import { Cell } from '@/models/Cell'
 
 interface CellProps {
   cell: Cell
+  selected: Boolean
+  click: (cell: Cell) => void
 }
 
 const props: CellProps = defineProps<CellProps>()
-
 </script>
